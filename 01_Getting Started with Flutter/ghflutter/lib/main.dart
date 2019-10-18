28f7859290289fe8f7b747acead1b0b866c760f8
htaiwan <htaiwan@oath.com>
2019-10-18 14:52:47 +0800

    [01] Widgets

--- 01_Getting Started with Flutter/ghflutter/lib/main.dart
+++ 01_Getting Started with Flutter/ghflutter/lib/main.dart
@@ -9,14 +9,27 @@ class GHFlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: Strings.appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(Strings.appTitle),
-        ),
-        body: Center(
-          child: Text(Strings.appTitle),
-        ),
-      ),
+      home: GHFlutter(),
     );    
+  }
+}
+
+class GHFlutter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GHFlutterState();
+  }
+}
+
+class GHFlutterState extends State<GHFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    // A Scaffold is a container for material design widgets. It acts as the root of a widget hierarchy.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appTitle) ,
+        ),
+        body: Text(Strings.appTitle),
+    );
   }
 }
