884c7f6bdf94dfc35134421ac1f5f5d4398b9110
htaiwan <htaiwan@oath.com>
2019-10-19 07:02:29 +0800

    [02] Returning a Value

--- 02_Flutter Navigation/ghnavigation-starter/lib/memberwidget.dart
+++ 02_Flutter Navigation/ghnavigation-starter/lib/memberwidget.dart
@@ -35,9 +35,55 @@ class MemberState extends State<MemberWidget> {
             onPressed: () {
               Navigator.pop(context);
             },
+          ),
+          RaisedButton(
+            child: Text('Press ME'),
+            onPressed: (){
+              _showOKScreen(context);
+            },
           )
         ],
       )
     );
   }
+
+_showOKScreen(BuildContext context) async {
+  //  push a new MaterialPageRoute onto the stack with a type parameter of bool
+  //  using await when pushing the new route, which waits until the route is popped.
+  //  the bool type parameter in MaterialPageRoute, which you can replace with any other type you want coming back from the route
+  bool value = await Navigator.push(context, MaterialPageRoute<bool>(builder: (BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(children: <Widget>[
+        // In the calls to pop(), 
+        // you pass a return value of true if the user tapped the “OK” text on the screen, and false if the user tapped “NOT OK”. 
+        // If the user presses the back button instead, the value returned is null.
+        GestureDetector(
+          child: Text('OK'),
+          onTap: () {
+            Navigator.pop(context, true);
+          }),
+        GestureDetector(
+          child: Text('Not OK'),
+          onTap: () {
+            Navigator.pop(context, false);
+          })
+      ]));
+  }));
+
+  var alert = AlertDialog(
+    content: Text((value != null && value) ? "OK was pressed" : "NOT OK or BACK was pressed"),
+    actions: <Widget>[
+      FlatButton(
+        child: Text('OK'),
+        onPressed: () {
+          Navigator.pop(context);
+        })
+    ],
+  );
+
+  //  call showDialog() to show the alert
+  showDialog(context: context, child: alert);
+}
+
 }
