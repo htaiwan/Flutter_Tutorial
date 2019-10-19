ca1d3a6a1039dd642b207ed843ac2f35514747a8
htaiwan <htaiwan@oath.com>
2019-10-19 08:20:23 +0800

    [03] Implement the button row

--- 03_Building layouts/building_layout/lib/main.dart
+++ 03_Building layouts/building_layout/lib/main.dart
@@ -52,7 +52,44 @@ class MyApp extends StatelessWidget {
 
     );
 
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, '電話'),
+          _buildButtonColumn(color, Icons.near_me, '地圖'),
+          _buildButtonColumn(color, Icons.share, '分享'),
+        ],
+      ),
+    );
+
     
+  }
+
+  // create a private helper method named buildButtonColumn(), 
+  // which takes a color, an Icon and Text, and returns a column with its widgets painted in the given color.
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      // 靠越近越好
+      mainAxisSize: MainAxisSize.min,
+      // 置中擺放
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 12,
+              color: color)
+          ),
+        )
+      ],
+    );
   }
 }
 
