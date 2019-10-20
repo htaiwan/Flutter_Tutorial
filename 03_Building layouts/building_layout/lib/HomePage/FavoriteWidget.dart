360a75926495557429c9cd537d3edd1fa9ba208e
htaiwan <htaiwan@oath.com>
2019-10-21 07:58:22 +0800

    [03] Subclass State

--- 03_Building layouts/building_layout/lib/HomePage/FavoriteWidget.dart
+++ 03_Building layouts/building_layout/lib/HomePage/FavoriteWidget.dart
@@ -1,8 +1,53 @@
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class FavoriteWidget extends StatefulWidget {
 
   @override
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            // 將邏輯跟UI進行binding
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorive,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            // 將邏輯跟UI進行binding
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorive() {
+    // 觸發UI更新
+    setState(() {
+      if (_isFavorited) {
+        // 從yes -> no
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
 }
